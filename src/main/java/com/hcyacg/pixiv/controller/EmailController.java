24c0934375aeebf39c5d3596ff9ba679e8df1ef4
牧瀬くりす <243462032@qq.com>
2021-03-18 19:20:37 +0800

    添加Swagger2自动生成接口文档，适配MySql 8.0

--- src/main/java/com/hcyacg/pixiv/controller/EmailController.java
+++ src/main/java/com/hcyacg/pixiv/controller/EmailController.java
@@ -2,6 +2,10 @@ package com.hcyacg.pixiv.controller;
 
 import com.hcyacg.pixiv.dto.Result;
 import com.hcyacg.pixiv.service.EmailService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
  * @Date 2020/9/19 0:01
  * @Desc 邮箱
  */
+@Api(value = "邮箱业务接口",tags = "处理邮箱相关操作")
 @RestController
 @RequestMapping("emails")
 public class EmailController {
@@ -18,24 +23,41 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+    @ApiOperation(value = "发送注册验证码",notes = "发送注册验证码")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "email", value = "注册邮箱", required = true, paramType = "body", dataType = "string"),
+    })
     @RequestMapping(value = "code", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Result sendCode(@RequestParam(value = "email") String email) {
         return emailService.code(email);
     }
 
+    @ApiOperation(value = "发送验证码",notes = "发送验证码")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "authorization", value = "token", required = true, paramType = "header", dataType = "string"),
+    })
     @RequestMapping(value = "updateCode", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Result updateCode(@RequestHeader(value = "authorization") String authorization) {
         return emailService.updateCode(authorization);
     }
 
+
+    @ApiOperation(value = "发送更新邮箱的验证码",notes = "发送更新邮箱的验证码")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "authorization", value = "token", required = true, paramType = "header", dataType = "string"),
+            @ApiImplicitParam(name = "email", value = "邮箱", required = true, paramType = "body", dataType = "string"),
+    })
     @RequestMapping(value = "changeEmailCode", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Result changeEmailCode(@RequestHeader(value = "authorization") String authorization,@RequestParam(value = "email") String email) {
         return emailService.changeEmailCode(authorization,email);
     }
 
+    @ApiOperation(value = "发送忘记密码的验证码",notes = "发送忘记密码的验证码")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "email", value = "邮箱", required = true, paramType = "body", dataType = "string"),
+    })
     @RequestMapping(value = "changeForgetCode", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Result changePassWordCodeForForget(@RequestParam(value = "email") String email) {
-        System.out.println(email);
         return emailService.changePassWordCodeForForget(email);
     }
 }
--- src/main/java/com/hcyacg/pixiv/controller/OrderController.java
+++ src/main/java/com/hcyacg/pixiv/controller/OrderController.java
@@ -2,6 +2,10 @@ package com.hcyacg.pixiv.controller;
 
 import com.hcyacg.pixiv.dto.Result;
 import com.hcyacg.pixiv.service.OrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +17,7 @@ import java.util.concurrent.ExecutionException;
  * @Date 2020/10/19 16:39
  * @Desc
  */
+@Api(value = "订单业务接口",tags = "处理订单相关操作")
 @RestController
 @RequestMapping("orders")
 public class OrderController {
@@ -21,6 +26,11 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @ApiOperation(value = "获取订单详情",notes = "获取订单详情")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "authorization", value = "token", required = true, paramType = "header", dataType = "string"),
+            @ApiImplicitParam(name = "trade", value = "订单号", required = true, paramType = "query", dataType = "string"),
+    })
     @RequestMapping(value = "info", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Result detail( @RequestHeader(value = "authorization",required = false) String authorization, @RequestParam(value = "trade",required = false) String  trade) throws ExecutionException, InterruptedException {
         return orderService.getOrders(trade,authorization);
